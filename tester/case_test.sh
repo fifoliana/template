b86127f572fb40f323a62f23400b2fc0306e4c14
mikandri <mikandri@student.42antananarivo.mg>
2025-10-22 16:09:00 +0300

    refactor : case_test.sh add readability and flexibility

--- tester/case_test.sh
+++ tester/case_test.sh
@@ -1,7 +1,13 @@
 set -e # stop the script when a command (line) fails
-g++ code.cpp -o code
-g++ gen.cpp -o gen
-g++ brute.cpp -o brute
+
+gen_src = gen.cpp
+brute_src = brute.cpp
+code_src = code.cpp
+
+g++ $code_src -o code
+g++ $gen_src -o gen
+g++ $brute_src -o brute
+
 for((i = 1; ; ++i)); do
     ./gen $i > input_file
     ./code < input_file > myAnswer
@@ -9,6 +15,7 @@ for((i = 1; ; ++i)); do
     diff -Z myAnswer correctAnswer > /dev/null || break
     echo "Passed test: "  $i
 done
+
 echo "WA on the following test:"
 cat input_file
 echo "Your answer is:"
