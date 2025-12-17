658269012d0dd82ea7986076a18836d467e1db01
Your Name <you@exemple.com>
2025-12-18 00:05:20 +0300

    move the custom hash for unordered set and map from main template to file, because unused, prefer ordered ones

--- template.cpp
+++ template.cpp
@@ -12,27 +12,6 @@ using namespace std;
 typedef long long ll;
 #define endl "\n"
 
-struct custom_hash {
-	static uint64_t splitmix64(uint64_t x) {
-		x += 0x9e3779b97f4a7c15;
-		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-		return x ^ (x >> 31);
-	}
-
-	size_t operator()(uint64_t x) const {
-		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-		return splitmix64(x + FIXED_RANDOM);
-	}
-
-	size_t operator()(pair<uint64_t,uint64_t> x) const {
-		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-		return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);
-	}
-};
-
-// unordered_map<int, pair<int,int>, custom_hash>
-
 template <typename T>
 void	print(T cont, ostream & os)
 {
