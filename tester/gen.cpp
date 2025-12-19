da55c24c78a79b8b5896247cc731d6e2d0ba392a
Your Name <you@exemple.com>
2025-12-19 22:09:08 +0300

    feat : make the generator more flexible

--- tester/gen.cpp
+++ tester/gen.cpp
@@ -18,12 +18,35 @@ void rand_arr(ll n, ll a, ll b)
 	cout << endl;
 }
 
+// for stress test : just one test case per execution but multiple executions using case_test.sh
 int main(int argc, char const *argv[])
 {
 	(void) argc;
 
-	int t;
+	int seed;
 
-	t = atoi(argv[1]);
+	seed = atoi(argv[1]);
+	srand(seed);
+
+	//number of test cases
+	cout << 1 << endl;
+
+	// code here to generate input
+
 	return 0;
+}
+
+// for time test : just one execution but with many test cases in one execution
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+
+	srand(time(0));
+	int t = stoi(argv[1]);
+	cout << t << endl;
+
+	for (int i = 0; i < t; ++i)
+	{
+		// code here to generate input
+	}
 }
