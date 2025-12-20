bbbeab6a5b98f74efa33926e2ea4850673fadd49
Your Name <you@exemple.com>
2025-12-20 23:37:34 +0300

    add a tester with checker for multiple possible solutions and rearrange the tester inside two folder

---
+++ tester/multiple_possible_solution/case_test.sh
@@ -0,0 +1,26 @@
+set -e # stop the script when a command (line) fails
+
+gen_src=gen.cpp
+brute_src=2180C_brute.cpp
+code_src=2180C_upsolve_but_high_rated.cpp
+checker=checker.cpp
+
+g++ $code_src -o code
+g++ $gen_src -o gen
+g++ $brute_src -o brute
+g++ $checker -o checker
+
+for((i = 1; ; ++i)); do
+    ./gen $i > input_file
+    ./code < input_file > myAnswer
+    ./brute < input_file > correctAnswer
+    ./checker || break
+    echo "Passed test: "  $i
+done
+
+echo "WA on the following test:"
+cat input_file
+echo "Your answer is:"
+cat myAnswer
+echo "Correct answer is:"
+cat correctAnswer
---
+++ tester/multiple_possible_solution/checker.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(void)
+{
+	ifstream in("input_file");
+	ifstream out("myAnswer");
+	ifstream brute("correctAnswer");
+
+	int t;
+
+	in >> t;
+
+	for (int i = 0; i < t; ++i)
+	{
+		// read the input (in >> ...)
+		// read the answer (out >> ...)
+		// read the brute force solution (brute >> ...)
+
+		// check the propreties that make the answer correct or not
+
+		// if not correct return a non-zero value : return 1, with eventually a message
+
+	}
+	return (0);
+
+}
